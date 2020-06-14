abd9569d931dc178d06562ae83e10a205693e0c0
shahab <ranjbaryshahab@gmai.com>
2020-06-14 22:40:32 +0430

    Initial commit

---
+++ src/main/java/ir/shamsipour/softwareenginnering/project/shahabranjbary/batfall/BatfallApplication.java
@@ -0,0 +1,13 @@
+package ir.shamsipour.softwareenginnering.project.shahabranjbary.batfall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatfallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatfallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/shamsipour/softwareenginnering/project/shahabranjbary/batfall/BatfallApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.shamsipour.softwareenginnering.project.shahabranjbary.batfall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatfallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
